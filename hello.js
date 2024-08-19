e92d06032be58a0b714425f9891c8515e5e79407
ilean-github <ilean.83@gmail.com>
2024-08-19 20:52:12 +0200

    mit test

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from Spain. This is a test for MIT course!";
 console.log(hello());
 exports.hello = hello;
 
